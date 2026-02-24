aebe0b7d2837026e4e636cee52fa6194b2475183
Mehdi Nabhani <mehdi@nabhani.de>
2026-02-24 15:22:14 +0100

    chore: update dependencies and configure dev ports

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: { port: 5265 },
   plugins: [react()],
   test: {
     globals: true,
